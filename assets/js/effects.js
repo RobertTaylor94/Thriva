c701a875d11db361fc254bf3e16b7dab79247519
Robert Taylor <rjt903@gmail.com>
2023-02-02 16:00:31 +0000

    Animated speech bubble on page load

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -2,11 +2,13 @@
 let bubbleOne = $("#bubble-one");
 let bubbleTwo = $("#bubble-two");
 let bubbleThree = $("#bubble-three");
+let speechBubble= $(".speech-bubble");
 
 function loadQuote() {
     bubbleOne.delay(500).fadeIn();
     bubbleTwo.delay(1000).fadeIn();
     bubbleThree.delay(1500).fadeIn();
+    speechBubble.delay(2000).fadeIn();
 }
 
 loadQuote();
