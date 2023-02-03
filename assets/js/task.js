59adebdaeeb1cb2987bf3a0535c8cf8940f9f340
Robert Taylor <rjt903@gmail.com>
2023-02-03 15:31:13 +0000

    Added js script for task functionality

---
+++ assets/js/task.js
@@ -0,0 +1,15 @@
+//document selectors
+let addTaskBtn = $(".add-button");
+
+function addTask(event) {
+    event.preventDefault();
+
+    let task = $(".task-input")
+}
+
+function saveTask() {
+
+}
+
+//save and display task on button press
+addTaskBtn.on("click", addTask);
