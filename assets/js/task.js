f546b58cce476170146621cf7e976221843bbfe8
Robert Taylor <rjt903@gmail.com>
2023-02-03 16:09:19 +0000

    display task from user input

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,10 +1,25 @@
 //document selectors
-let addTaskBtn = $(".add-button");
+let addTaskBtn = $("#add-button");
+let taskInput = $(".task-input");
+let taskOutput = $("#task-output");
+let taskForm = $("#task-form");
+let taskDisplay = $("#task-display");
+let taskText = $("#daily-task");
+let progressBar = $("#progress-bar");
 
 function addTask(event) {
     event.preventDefault();
 
-    let task = $(".task-input")
+    var task = taskInput.val().trim();
+    console.log(task);
+    taskText.val(task);
+    taskInput.val("");
+
+    taskForm.css("display", "none");
+    taskDisplay.css("display", "block");
+    addTaskBtn.css("display", "none");
+    progressBar.css("display", "block");
+    taskText.text(task);
 }
 
 function saveTask() {
