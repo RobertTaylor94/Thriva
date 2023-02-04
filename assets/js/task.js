59ee6592478ea7992b0de8aeb3352101ade39287
Robert Taylor <rjt903@gmail.com>
2023-02-04 13:35:49 +0000

    import moment.js

--- assets/js/task.js
+++ assets/js/task.js
@@ -7,14 +7,20 @@ let taskDisplay = $("#task-display");
 let taskText = $("#daily-task");
 let progressBar = $("#progress-bar");
 
+let todaysDate = moment().format("DD MM YYYY")
+let timerEnd = moment().hour(20).minutes(0)
+
+console.log(timerEnd)
+
 function addTask(event) {
     event.preventDefault();
-
+    //get user input from text field
     var task = taskInput.val().trim();
     console.log(task);
     taskText.val(task);
     taskInput.val("");
-
+    
+    //hide input form and show inputted task and timer
     taskForm.css("display", "none");
     taskDisplay.css("display", "block");
     addTaskBtn.css("display", "none");
@@ -22,7 +28,11 @@ function addTask(event) {
     taskText.text(task);
 }
 
-function saveTask() {
+function saveTask(task) {
+
+}
+
+function updateTimer() {
 
 }
 
