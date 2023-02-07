383e74e0200335199ba1e621ec0b6b0fbbbe0639
Robert Taylor <rjt903@gmail.com>
2023-02-07 16:45:54 +0000

    Commented code

--- assets/js/task.js
+++ assets/js/task.js
@@ -25,24 +25,31 @@ function addTask(event) {
 }
 
 function saveTask(task) {
+    //Store user input to local storage
     localStorage.setItem("task", JSON.stringify(task));
 }
 
 function retrieveTask() {
+    //retrieve user task from local storage
     task = JSON.parse(localStorage.getItem("task"));
 
+    //if no task saved then display the form for submitting a new task for the day
     if (!task) {
         taskForm.css("display", "block");
         addTaskBtn.css("display", "block");
         taskDisplay.css("display", "none");
     } else {
+        //if a task has been saved show the task and hide the form
         taskForm.css("display", "none");
         taskDisplay.css("display", "block");
         addTaskBtn.css("display", "none");
         progressBar.css("display", "none");
         taskText.text(task);
     }
+}
 
+function clearTask() {
+    localStorage.removeItem("task");
 }
 
 //save and display task on button press
